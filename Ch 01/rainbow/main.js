fef0efccc8176c5f1ef4754ee685d82b43b62170
Sergio-Feliciano Mendoza-Barrera <s.f.m@ieee.org>
2018-11-23 14:20:37 -0600

    [Chapter 1] :: JS first test

--- Ch 01/rainbow/main.js
+++ Ch 01/rainbow/main.js
@@ -3,6 +3,6 @@ const btn = document.getElementById('button');
 const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
 
 function change() {
-  document.body.style.background = rainbow[Math.floor(7*Math.random())];
+    document.body.style.background = rainbow[Math.floor(7*Math.random())];
 }
 btn.addEventListener('click', change);
